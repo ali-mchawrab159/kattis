8b7c9ddbd50d767497603a3bf736cfe5833fa004
Ali Mchawrab <alimchawrab159@gmail.com>
2025-12-19 22:26:38 +0100

    added new test types

--- tests/test_r2.py
+++ tests/test_r2.py
@@ -1,9 +1,15 @@
+import pytest
 from kattis.r2 import r2
 
 
-def test_r2_basic():
-    assert r2(11, 15) == 19
+@pytest.mark.parametrize(
+    "r1, s, expected",
+    [
+        (11, 15, 19),
+        (0, 10, 20),
+        (-5, -3, -1),
+    ],
+)
+def test_r2_parametrized(r1, s, expected):
+    assert r2(r1, s) == expected
 
-
-def test_r2_negative():
-    assert r2(-5, -3) == -1
